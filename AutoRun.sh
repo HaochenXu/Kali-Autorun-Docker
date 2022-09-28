ab0094cdddff9d58e6b6264b47a3c166758ee4e5
HaochenXu <62594109+HaochenXu@users.noreply.github.com>
2022-09-28 12:59:28 +0800

    Update AutoRun.sh

--- AutoRun.sh
+++ AutoRun.sh
@@ -7,7 +7,7 @@ echo "deb-src http://mirrors.tuna.tsinghua.edu.cn/kali kali-rolling main contrib
 cd //
 apt update
 apt upgrade -y
-apt install python3 python3-pip python3-wheel python3-setuptools openssh-server gcc
+apt install python3 python3-pip python3-wheel python3-setuptools openssh-server gcc nano
 
 tar -xzf openjdk.tar.gz
 echo "JAVA_HOME=/jdk-17.0.4.1+1" >> /etc/profile
